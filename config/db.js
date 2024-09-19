6a9e3276de25c5a10137cd4018412ff7b95f9269
Natanael <natancnascimento88@gmail.com>
2024-09-19 10:47:14 -0300

    modificado projeto Tennis Store

---
+++ config/db.js
@@ -0,0 +1,25 @@
+const mysql = require('mysql2'); //importar  pacote mysql2 para conectar
+
+// Exibe as variáveis de ambiente carregadas
+console.log('DB_HOST:', process.env.DB_HOST);
+console.log('DB_USER:', process.env.DB_USER);
+console.log('DB_PASS:', process.env.DB_PASS);
+console.log('DB_NAME:', process.env.DB_NAME); 
+
+
+const db = mysql.createConnection({
+    host:process.env.DB_HOST, //endereço do servidor de Banco de Dados
+    user:process.env.DB_USER, //nome de usuario para acessar o Banco de Dados
+    password:process.env.DB_PASS, //senha do usuario para acessar o Banco de Dados
+    database:process.env.DB_NAME //nome do Banco de Dados
+});
+
+db.connect((err) => {
+    if(err){
+        console.error('erro ao conectar ao Banco de Dados.', err); //exibe a mensagem de erro
+    return;
+}
+    console.log(`conectado ao Banco de Dados Mysql, ${process.env.DB_NAME}`); //exbe a mensagem de sucesso
+});
+
+module.exports=db;
