e0452a89db29c1cbef0ba435a0b3e62e34330791
Natanael <natancnascimento88@gmail.com>
2024-09-23 10:50:53 -0300

    modificado projeto Tennis Store

--- controllers/productController.js
+++ controllers/productController.js
@@ -12,6 +12,85 @@ const getAllProduct = (req, res) => {
     });
 };
 
+const addProduct = (req, res) => {
+    const {brand, size, color, price} = req.body;
+    db.query(
+        'INSERT INTO product (brand, size, color, price) VALUES (?,?,?,?)',
+        [brand, size, color, price],
+        (err,results) => {
+            if(err) {
+                console.error('erro ao adicionar produtos', err);
+                res.status(500).send('erro ao adicionar produtos');
+                return;
+            }
+            res.status(201).send('produtos adicionar com sucesso');
+        }
+    );
+};
+
+const updateProductPut = (req, res) => {
+    const {id} = req.params;
+    const {brand, size, color, price} = req.body;
+    db.query(
+        'UPDATE product SET brand=?, size=?, color=?, price=? WHERE id=?',
+        [brand, size, color, price, id],
+        (err, results) => {
+            if(err) {
+                console.error('erro ao adicionar produto', err);
+                res.status(500).send('erro ao adiconar produto');
+                return;
+            }
+            res.send('produto atualizado com sucesso')
+        }
+    );
+};
+
+const updateProductPatch = (req, res) => {
+    const{id} = req.params;
+    const fields = req.body;
+    const query = [];
+    const values = [];
+
+    for(const[key,value] of Object.entries(fields)) {
+        query.push (`${key} = ?`);
+        values.push(value);
+    }
+    values.push(id);
+
+    db.query(
+        `UPDATE product set ${query.join(',')} WHERE id = ?`,
+        values,
+        (err, results) => {
+            if(err) {
+                console.error('erro ao adicionar produto', err);
+                res.status(500).send('erro ao adicionar produto');
+                return;
+            }
+            res.send('produtos atualizado com sucesso');
+        }
+        
+    );
+};
+
+const deleteProduct = (req,res) => {
+    const{id} = req.params;
+    db.query('DELETE FROM Products WHERE id = ?',[id],
+        (err, results) => {
+            if(err) {
+                console.error('erro ao adicionar produto', err);
+                res.status(500).send('erro ao deletar produto');
+                return;
+            }
+            res.send('produto deletado com sucesso');
+        }
+    );
+};
+
+
 module.exports = {
-    getAllProduct
+    getAllProduct,
+    addProduct,
+    updateProductPut,
+    updateProductPatch,
+    deleteProduct
 }
--- routes/product.js
+++ routes/product.js
@@ -4,5 +4,12 @@ const productController = require('../controllers/productController');
 
 router.get('/', productController.getAllProduct);
 
+router.post('/', productController.addProduct);
+
+router.put('/:id', productController.updateProductPut);
+
+router.patch('/:id', productController.updateProductPatch);
+
+router.delete('/:id', productController.deleteProduct);
 
 module.exports = router;
